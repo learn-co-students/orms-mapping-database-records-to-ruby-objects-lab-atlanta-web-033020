dcbb65bc5a4fd195018b14b7d1589bcf7ba3103b
rpdecks <rpdecks@gmail.com>
2020-04-09 21:43:16 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,94 @@
+require 'pry'
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+    SQL
+    all = []
+    DB[:conn].execute(sql).each do |row|
+      # binding.pry
+      all << new_from_db(row)
+    end
+    all
+  end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE students.grade = 9
+    SQL
+    ninth = DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE students.grade < 12
+    SQL
+    # binding.pry
+    under_12 = DB[:conn].execute(sql)
+    under_12.first
+  end
+
+  def self.first_X_students_in_grade_10(count)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE students.grade = 10
+      LIMIT #{count}
+    SQL
+    x = []
+    DB[:conn].execute(sql).each do |row|
+      x << new_from_db(row)
+    end
+    x
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE students.grade = 10
+      ORDER BY students.id ASC
+      LIMIT 1
+    SQL
+    first = DB[:conn].execute(sql).each.first
+    new_from_db(first)
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE students.grade = #{grade}
+    SQL
+    all_in_grade = []
+    DB[:conn].execute(sql).each do |row|
+      all_in_grade << new_from_db(row)
+    end
+    all_in_grade
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE students.name = name
+    SQL
+    row = DB[:conn].execute(sql)
+    found = new_from_db(row.first)
+    found
   end
   
   def save
@@ -32,7 +108,6 @@ class Student
       grade TEXT
     )
     SQL
-
     DB[:conn].execute(sql)
   end
 
@@ -40,4 +115,5 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
 end
